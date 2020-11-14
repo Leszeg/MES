7b62acf12a433e14baeb2b4985134b7e8f4445a8
Leszeg <mazix1480@gmail.com>
2020-11-14 10:47:11 +0100

    Działająca macierz H dla jednego elementu

--- MES/Element.py
+++ MES/Element.py
@@ -54,7 +54,7 @@ class Element:
 
     def jacobian(self):
         x = [0, 4, 4, 0]
-        y = [0, 0, 4, 4]
+        y = [0, 0, 6, 6]
         derivativeEta = np.zeros((4, 4), float)
         derivativeKsi = np.zeros((4, 4), float)
 
@@ -66,7 +66,6 @@ class Element:
 
         eta = [self.nodes[0].eta, self.nodes[1].eta, self.nodes[3].eta, self.nodes[2].eta]
         ksi = [self.nodes[0].ksi, self.nodes[1].ksi, self.nodes[3].ksi, self.nodes[2].ksi]
-
 
         for i in range(0, self.nodes_count):
             for j in range(0, self.nodes_count):
@@ -109,8 +108,6 @@ class Element:
                     inv_jacobian[k][i] = jacobian[j][i] / determinant[i]
                 elif k == 1 or k == 2:
                     inv_jacobian[k][i] = -jacobian[j][i] / determinant[i]
-                    if inv_jacobian[k][i] == -0.0:
-                        inv_jacobian[k][i] = 0.0
                 k += 1
             k = 0
         print("///////////X////////////")
@@ -145,3 +142,139 @@ class Element:
         print('////////// INVERSE JACOBIAN //////////')
         for i in range(self.nodes_count):
             print(inv_jacobian[i])
+
+        dN_dx = np.zeros((4, 4), float)
+        for i in range(0, self.nodes_count):
+            k = 0
+            for j in range(2):
+                for l in range(2):
+                    dN_dx[i][k] = ((1 / determinant[0]) * (
+                            derivativeKsi[i][k] * jacobian[1][k] + derivativeEta[i][k] * (jacobian[3][k])))
+                    k += 1
+
+        print("////////// Pochodna funkcji kształtu X //////////")
+        for i in range(self.nodes_count):
+            print(dN_dx[i])
+        return jacobian, derivativeEta, derivativeKsi, determinant
+
+    def H_matrix(self):
+
+        data = self.jacobian()
+        jacobian = data[0]
+        derivativeEta = data[1]
+        derivativeKsi = data[2]
+        determinant = data[3]
+
+        jacSize = 2
+        One = np.zeros((jacSize, jacSize), float)
+        Two = np.zeros((jacSize, jacSize), float)
+        Three = np.zeros((jacSize, jacSize), float)
+        Four = np.zeros((jacSize, jacSize), float)
+        k = 0
+
+        for i in range(jacSize):
+            for j in range(jacSize):
+                One[j][i] = jacobian[k][0]
+                Two[j][i] = jacobian[k][1]
+                Three[j][i] = jacobian[k][2]
+                Four[j][i] = jacobian[k][3]
+                k += 1
+
+        dNdX = np.zeros((4, 4), float)
+        dNdY = np.zeros((4, 4), float)
+
+        dNdX[0][0] = (1 / determinant[0]) * (One[1][0] * derivativeEta[0][0] + One[1][1] * derivativeEta[0][0])
+        dNdX[0][1] = (1 / determinant[0]) * (One[1][0] * derivativeEta[0][1] + One[1][1] * derivativeEta[0][1])
+        dNdX[0][2] = (1 / determinant[0]) * (One[1][0] * derivativeEta[0][2] + One[1][1] * derivativeEta[0][2])
+        dNdX[0][3] = (1 / determinant[0]) * (One[1][0] * derivativeEta[0][3] + One[1][1] * derivativeEta[0][3])
+
+        dNdX[1][0] = (1 / determinant[1]) * (Two[1][0] * derivativeEta[1][0] + Two[1][1] * derivativeEta[1][0])
+        dNdX[1][1] = (1 / determinant[1]) * (Two[1][0] * derivativeEta[1][1] + Two[1][1] * derivativeEta[1][1])
+        dNdX[1][2] = (1 / determinant[1]) * (Two[1][0] * derivativeEta[1][2] + Two[1][1] * derivativeEta[1][2])
+        dNdX[1][3] = (1 / determinant[1]) * (Two[1][0] * derivativeEta[1][3] + Two[1][1] * derivativeEta[1][3])
+
+        dNdX[2][0] = (1 / determinant[2]) * (
+                Three[1][0] * derivativeEta[2][0] + Three[1][1] * derivativeEta[2][0])
+        dNdX[2][1] = (1 / determinant[2]) * (
+                Three[1][0] * derivativeEta[2][1] + Three[1][1] * derivativeEta[2][1])
+        dNdX[2][2] = (1 / determinant[2]) * (
+                Three[1][0] * derivativeEta[2][2] + Three[1][1] * derivativeEta[2][2])
+        dNdX[2][3] = (1 / determinant[2]) * (
+                Three[1][0] * derivativeEta[2][3] + Three[1][1] * derivativeEta[2][3])
+
+        dNdX[3][0] = (1 / determinant[3]) * (Four[1][0] * derivativeEta[3][0] + Four[1][1] * derivativeEta[3][0])
+        dNdX[3][1] = (1 / determinant[3]) * (Four[1][0] * derivativeEta[3][1] + Four[1][1] * derivativeEta[3][1])
+        dNdX[3][2] = (1 / determinant[3]) * (Four[1][0] * derivativeEta[3][2] + Four[1][1] * derivativeEta[3][2])
+        dNdX[3][3] = (1 / determinant[3]) * (Four[1][0] * derivativeEta[3][3] + Four[1][1] * derivativeEta[3][3])
+        ##################################################################################################
+        dNdY[0][0] = (1 / determinant[0]) * (One[0][0] * derivativeKsi[0][0] + One[0][1] * derivativeKsi[0][0])
+        dNdY[0][1] = (1 / determinant[0]) * (One[0][0] * derivativeKsi[0][1] + One[0][1] * derivativeKsi[0][1])
+        dNdY[0][2] = (1 / determinant[0]) * (One[0][0] * derivativeKsi[0][2] + One[0][1] * derivativeKsi[0][2])
+        dNdY[0][3] = (1 / determinant[0]) * (One[0][0] * derivativeKsi[0][3] + One[0][1] * derivativeKsi[0][3])
+
+        dNdY[1][0] = (1 / determinant[1]) * (Two[0][0] * derivativeKsi[1][0] + Two[0][1] * derivativeKsi[1][0])
+        dNdY[1][1] = (1 / determinant[1]) * (Two[0][0] * derivativeKsi[1][1] + Two[0][1] * derivativeKsi[1][1])
+        dNdY[1][2] = (1 / determinant[1]) * (Two[0][0] * derivativeKsi[1][2] + Two[0][1] * derivativeKsi[1][2])
+        dNdY[1][3] = (1 / determinant[1]) * (Two[0][0] * derivativeKsi[1][3] + Two[0][1] * derivativeKsi[1][3])
+
+        dNdY[2][0] = (1 / determinant[2]) * (
+                Three[0][0] * derivativeKsi[2][0] + Three[0][1] * derivativeKsi[2][0])
+        dNdY[2][1] = (1 / determinant[2]) * (
+                Three[0][0] * derivativeKsi[2][1] + Three[0][1] * derivativeKsi[2][1])
+        dNdY[2][2] = (1 / determinant[2]) * (
+                Three[0][0] * derivativeKsi[2][2] + Three[0][1] * derivativeKsi[2][2])
+        dNdY[2][3] = (1 / determinant[2]) * (
+                Three[0][0] * derivativeKsi[2][3] + Three[0][1] * derivativeKsi[2][3])
+
+        dNdY[3][0] = (1 / determinant[3]) * (Four[0][0] * derivativeKsi[3][0] + Four[0][1] * derivativeKsi[3][0])
+        dNdY[3][1] = (1 / determinant[3]) * (Four[0][0] * derivativeKsi[3][1] + Four[0][1] * derivativeKsi[3][1])
+        dNdY[3][2] = (1 / determinant[3]) * (Four[0][0] * derivativeKsi[3][2] + Four[0][1] * derivativeKsi[3][2])
+        dNdY[3][3] = (1 / determinant[3]) * (Four[0][0] * derivativeKsi[3][3] + Four[0][1] * derivativeKsi[3][3])
+
+        dNdX1 = np.zeros((4, 4), float)
+        dNdY1 = np.zeros((4, 4), float)
+        dNdX2 = np.zeros((4, 4), float)
+        dNdY2 = np.zeros((4, 4), float)
+        dNdX3 = np.zeros((4, 4), float)
+        dNdY3 = np.zeros((4, 4), float)
+        dNdX4 = np.zeros((4, 4), float)
+        dNdY4 = np.zeros((4, 4), float)
+
+        for i in range(0, 4):
+            for j in range(0, 4):
+                dNdX1[i][j] = dNdX[0][i] * dNdX[0][j]
+                dNdY1[i][j] = dNdY[0][i] * dNdY[0][j]
+
+        for i in range(0, 4):
+            for j in range(0, 4):
+                dNdX2[i][j] = dNdX[1][i] * dNdX[1][j]
+                dNdY2[i][j] = dNdY[1][i] * dNdY[1][j]
+
+        for i in range(0, 4):
+            for j in range(0, 4):
+                dNdX3[i][j] = dNdX[2][i] * dNdX[2][j]
+                dNdY3[i][j] = dNdY[2][i] * dNdY[2][j]
+
+        for i in range(0, 4):
+            for j in range(0, 4):
+                dNdX4[i][j] = dNdX[3][i] * dNdX[3][j]
+                dNdY4[i][j] = dNdY[3][i] * dNdY[3][j]
+
+        for i in range(0, 4):
+            for j in range(0, 4):
+                dNdX1[i][j] += dNdY1[i][j]
+                dNdX2[i][j] += dNdY2[i][j]
+                dNdX3[i][j] += dNdY3[i][j]
+                dNdX4[i][j] += dNdY4[i][j]
+
+        H = np.zeros((4, 4), float)
+        for i in range(0, 4):
+            for j in range(0, 4):
+                H[i][j] = 30 * (dNdX1[i][j] + dNdX2[i][j] + dNdX3[i][j] + dNdX4[i][j]) * determinant[i]
+
+        print("\n\n")
+        print("////////// H //////////")
+        print(H[0])
+        print(H[1])
+        print(H[2])
+        print(H[3])
--- Main.py
+++ Main.py
@@ -66,9 +66,76 @@ def lab_2():
     print(element1.integral())
     print(element2.integral())
 
+def lab_3():
+    element = e.Element(4)
+    element.jacobian()
+
+def lab_4():
+    element = e.Element(4)
+    element.H_matrix()
+
+def lab_5():
+    # Czytam dane z pliku i ładuje do klasy
+    data = r.read()
+    global_data = g.GlobalData(data["W"], data["H"], data["nH"], data["nW"])
+
+    # Obliczam rzeczy potrzebne do obliczenia współrzędnych
+    d_x = global_data.W / (global_data.nW - 1)
+    d_y = global_data.H / (global_data.nH - 1)
+    i = int(global_data.nW - 1)
+    j = int(global_data.nH - 1)
+
+    # Ilości węzłów i elementów oraz inicjowanie pustych tablic
+    nN = global_data.nH * global_data.nW
+    nE = (global_data.nH - 1) * (global_data.nW - 1)
+    nodes = []
+    elements = []
+
+    # Tworzenie współrzędnych węzłów
+    for i1 in range(i + 1):
+        for j1 in range(j + 1):
+            nodes.append(n.Node(0, 0, round(i1 * d_x, 4), round(j1 * d_y, 4)))
+
+    # Tworzenie elementów
+    helpp = 0
+    help1 = 0
+    j = 0
+    for i in range(nE + int((global_data.nW) / 2)):
+        if helpp < global_data.nH - 1:
+            ID = []
+            ID.append(j)
+            ID.append(ID[0] + global_data.nH)
+            ID.append(ID[1] + 1)
+            ID.append(ID[0] + 1)
+            elements.append(e.Element(4))
+            elements[help1].kons(ID)
+            help1 += 1
+            helpp = helpp + 1
+            j = j + 1
+        else:
+            j = j + 1
+            helpp = 0
+
+    # Generuje siatkę złożoną z węzłów i elementów
+    fem_grid = f.FEM_Grid(nodes, elements)
+
+    # for i in range(nN):
+    #     print(fem_grid.n[i].x, " ", fem_grid.n[i].y)
+    #
+    # for i in range(nE):
+    #     print(f"Element {i}")
+    #     for j in range(4):
+    #         print(fem_grid.e[i].nodes_ID[j])
+
+
+    # Drukuje wykres siatki
+    Plot.plot(nN, nodes)
+    for i in range(nE + int((global_data.nW) / 2)):
+        elements[i].H_matrix()
+
 
 lab_1()
-lab_2()
-
-element = e.Element(4)
-element.jacobian()
+# lab_2()
+# lab_3()
+lab_4()
+# lab_5()
